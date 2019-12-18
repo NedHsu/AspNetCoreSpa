acea5080b6affe133e91cd5f84843f409cf018ac
Ned <Ned@RD07>
2019-12-18 16:44:39 +0800

    add @angular/material new vote module component

---
+++ src/AspNetCoreSpa.Web/ClientApp/src/app/vote/vote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'appc-vote',
+  templateUrl: './vote.component.html',
+  styleUrls: ['./vote.component.scss']
+})
+export class VoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/AspNetCoreSpa.Web/ClientApp/src/app/vote/vote.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VoteComponent } from './vote.component';
+
+
+
+@NgModule({
+  declarations: [VoteComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VoteModule { }
