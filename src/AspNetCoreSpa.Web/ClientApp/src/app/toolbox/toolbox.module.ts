03e5652fd68c6d8b417e30bfe9fcfdb0c6fe0ac1
Ned <Ned@RD07>
2019-12-19 18:23:17 +0800

    增加 toolbox, admin

---
+++ src/AspNetCoreSpa.Web/ClientApp/src/app/toolbox/toolbox.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolboxComponent } from './toolbox.component';
+
+
+
+@NgModule({
+  declarations: [ToolboxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolboxModule { }
